aea4f9b11a0feeda8e02645ef761510224742fdd
Rowell Heria <owieh@me.com>
2017-03-10 22:19:49 +0000

    basic app now working

---
+++ Web/App/boot.ts
@@ -0,0 +1,13 @@
+﻿import { Aurelia } from "aurelia-framework";
+declare const IS_DEV_BUILD: boolean; // The value is supplied by Webpack during the build
+
+export function configure(app: Aurelia) {
+
+    app.use.standardConfiguration();
+
+    if (IS_DEV_BUILD) {
+        app.use.developmentLogging();
+    }
+
+    app.start().then(a => { a.setRoot(); });
+}
---
+++ Web/webpack.config.js
@@ -0,0 +1,46 @@
+﻿var isDevBuild = process.argv.indexOf("--ennv.prod") < 0;
+var path = require("path");
+var webpack = require("webpack");
+var AureliaWebpackPlugin = require("aurelia-webpack-plugin");
+
+var bundleOutputDir = "./wwwroot/dist";
+var devPlugins = [
+    new webpack.SourceMapDevToolPlugin({
+        filename: "[file].map",
+        moduleFilenameTemplate: path.relative(bundleOutputDir, "[resourcePath]")
+    })
+];
+var prodPlugins = [
+    new webpack.optimize.UglifyJsPlugin()
+];
+
+module.exports = {
+    resolve: { extensions: [".js", ".ts"] },
+    entry: { "app": "aurelia-bootstrapper-webpack" },
+    output: {
+        path: path.join(__dirname, "wwwroot", "dist"),
+        publicPath: "/dist",
+        filename: "[name].js"
+    },
+    module: {
+        loaders: [
+            { test: /\.ts$/, include: /App/, loader: "ts-loader", query: { silent: true } },
+            { test: /\.html$/, loader: "html-loader" },
+            { test: /\.scss$/, loaders: [{ loader: "style-loader" }, { loader: "css-loader" }, { loader: "sass-loader" }] },
+            { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: "url-loader?limit=100000" },
+            { test: /\.json$/, loader: "json-loader" }
+        ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({ IS_DEV_BUILD: JSON.stringify(isDevBuild) }),
+        new webpack.DllReferencePlugin({
+            context: __dirname,
+            manifest: require("./wwwroot/dist/vendor-manifest.json")
+        }),
+        new AureliaWebpackPlugin({
+            root: path.resolve("./"),
+            src: path.resolve("./App"),
+            baseUrl: "/"
+        })
+    ].concat(isDevBuild ? devPlugins : prodPlugins)
+};
