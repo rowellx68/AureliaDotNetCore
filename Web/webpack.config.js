0a64d0c006a719dd4090d820fc4e866a0803f4ec
Rowell Heria <rowell.heria@answerdigital.com>
2017-03-24 16:14:33 +0000

    merged vendor webpack with default

--- Web/webpack.config.js
+++ Web/webpack.config.js
@@ -2,6 +2,8 @@
 var path = require("path");
 var webpack = require("webpack");
 var AureliaWebpackPlugin = require("aurelia-webpack-plugin");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var extractCSS = new ExtractTextPlugin("vendor.css");
 
 var bundleOutputDir = "./wwwroot/dist";
 var devPlugins = [
@@ -16,7 +18,15 @@ var prodPlugins = [
 
 module.exports = {
     resolve: { extensions: [".js", ".ts"] },
-    entry: { "app": "aurelia-bootstrapper-webpack" },
+    entry: { 
+        app: "aurelia-bootstrapper-webpack",
+        vendor: [
+            "aurelia-framework",
+            "aurelia-router",
+            "bootstrap/dist/css/bootstrap.css",
+            "font-awesome/css/font-awesome.css"
+        ]
+    },
     output: {
         path: path.join(__dirname, "wwwroot", "dist"),
         publicPath: "/dist",
@@ -28,15 +38,13 @@ module.exports = {
             { test: /\.html$/, loader: "html-loader" },
             { test: /\.scss$/, loaders: [{ loader: "style-loader" }, { loader: "css-loader" }, { loader: "sass-loader" }] },
             { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: "url-loader?limit=100000" },
-            { test: /\.json$/, loader: "json-loader" }
+            { test: /\.json$/, loader: "json-loader" },
+            { test: /\.css(\?|$)/, loader: extractCSS.extract(["css-loader"]) }
         ]
     },
     plugins: [
+        extractCSS,
         new webpack.DefinePlugin({ IS_DEV_BUILD: JSON.stringify(isDevBuild) }),
-        new webpack.DllReferencePlugin({
-            context: __dirname,
-            manifest: require("./wwwroot/dist/vendor-manifest.json")
-        }),
         new AureliaWebpackPlugin({
             root: path.resolve("./"),
             src: path.resolve("./App"),
